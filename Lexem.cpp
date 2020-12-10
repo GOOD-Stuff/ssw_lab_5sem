f172841eb8bfd9477ffc3d5deccef3dd4ddce0a3
StaryjWalk <60772603+StaryjWalk@users.noreply.github.com>
2020-12-10 17:12:04 +0300

    Update Lexem.cpp

--- Lexem.cpp
+++ Lexem.cpp
@@ -1,21 +1,13 @@
-//
-// Created by vovan on 28.09.2019.
-//
-
 #include "Lexem.h"
 
+const std::string& Lexem::GetName() const {
+    return name;
+}
 
-tokens Lexem::GetToken() const {
+Tokens Lexem::GetToken() const {
     return token;
 }
 
-
-/**
- * @brief Return line (from pascal file) of lexeme
- * @param none
- *
- * @return line of lexeme
- */
 int Lexem::GetLine() const {
     return line;
 }
