0738948f98176140bdb3920e74a6ba8b0207d300
StaryjWalk <60772603+StaryjWalk@users.noreply.github.com>
2020-12-10 17:16:33 +0300

    Update Syntax.h

--- Syntax.h
+++ Syntax.h
@@ -1,9 +1,5 @@
-//
-// Created by vovan on 28.09.2019.
-//
-
-#ifndef SYNTAX_H
-#define SYNTAX_H
+#ifndef LECS_PARS_SYNTAX_H
+#define LECS_PARS_SYNTAX_H
 #include <algorithm>
 #include <iostream>
 #include <chrono>
@@ -13,13 +9,6 @@
 #include "Lexem.h"
 #include "Variable.h"
 
-typedef struct tree_t {  // TODO: Here you have to rewrite to OOP
-    tree_t *left_node;
-    tree_t *rigth_node;
-    tree_t *parent_node; // XXX: Should we have/use link to parent node?
-    std::string value;
-} tree_t;
-
 
 class Syntax {
 public:
@@ -28,33 +17,29 @@ public:
     ~Syntax();
 private:
     using lex_it = std::vector<Lexem>::iterator; // alias of vector iterator
-    lex_it                          cursor;      // cursor in table of lexemes
-    std::vector<Lexem>              lex_table;   // our table of lexemes
+    lex_it  cursor;      // cursor in table of lexemes
+    std::vector<Lexem>  lex_table;   // our table of lexemes
     std::map<std::string, Variable> id_map;      // our table of identifiers
-    //tree_t                         *pascal_tree; // our syntax tree
+
 
     lex_it getNextLex(lex_it &iter);
     lex_it peekLex(int N, lex_it t_iter);
 
-    //tree_t *buildTreeStub(tree_t *t_tree, const std::string &node_name);
-    //tree_t *createNode(const std::string &node_name);
+
 
     int   expressionParse            (lex_it &t_iter);
     int   stateParse                 (lex_it &t_iter);
     int   compoundParse              (lex_it &t_iter);
-    int   vardpParse                 (lex_it &t_iter, tree_t *t_tree);
+    int   vardpParse                 (lex_it &t_iter);
     std::list<std::string> vardParse (lex_it &t_iter);
     int   blockParse                 (lex_it &t_iter);
     int   programParse               (lex_it &t_iter);
 
     void printError    (errors t_err, Lexem lex);
-    bool checkLexem    (const lex_it &t_iter, const tokens &t_tok);
+    bool checkLexem    (const lex_it &t_iter, const Tokens &t_tok);
     bool isVarExist    (const std::string &t_var_name);
     void updateVarTypes(const std::list<std::string> &t_var_list,const std::string &t_type_name);
-  //  void buildVarTree  (const std::list<std::string> &t_var_list, tree_t *t_tree);
 
-    //void createVarTree(tree_t *t_tree, tree_t *t_donor_tree, int lvl);
-    //void freeTreeNode (tree_t *&t_tree);
 };
 
 
