ea6b1d4e39e42e50d7e180e4dee498a86ccea08e
StaryjWalk <60772603+StaryjWalk@users.noreply.github.com>
2020-12-16 21:00:35 +0300

    Add files via upload

--- lab_01/src/main.cpp
+++ lab_01/src/main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include "lab_01.h"
 
-
-/**
- * to build call: g++ main.cpp task_01.cpp -o task
- * or make
- */
-
-
 int main() {
-	std::cout << "Ah, here we go again" << std::endl;
-	Parse2("test.p");
+    std::cout << "Ah, here we go again" << std::endl;
+    Parse_01("test.p");
 
-	return 0;
+    return 0;
 }
