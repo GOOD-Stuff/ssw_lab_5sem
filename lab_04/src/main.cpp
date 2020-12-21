cfedf4c3fbdebb43c2a57942a3c992e6d8301aa4
alfa-create <alasner@mail.ru>
2020-12-21 11:39:39 +0300

    test

--- lab_04/src/main.cpp
+++ lab_04/src/main.cpp
@@ -3,17 +3,12 @@
 #include <map>
 #include <string>
 #include "lab_04.h"
-/*#include "GenCode.cpp"
-#include "Lexem.cpp"
-#include "Lexer.cpp"
-#include "Syntax.cpp"
-#include "Tree.cpp"
-#include "Variable.cpp"*/
+
 
 
 int main() {
     std::cout << "Okey, okey, I get it" << std::endl;
-    Parse("C://Users/Pushistik/CLionProjects/lab_4/test.p");
+    Parse("C://Users/Pushistik/Documents/GitHub/labs/ssw_lab_5sem/lab_04/test.p");
 
     return 0;
 }
