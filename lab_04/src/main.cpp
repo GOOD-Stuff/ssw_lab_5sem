aa9aab1d1d2ada9345d1e58bd9fb61752d5ff974
Vasiliy Prokhorov <56435313+Stinger1000@users.noreply.github.com>
2020-12-21 19:29:19 +0300

    Update: main

--- lab_04/src/main.cpp
+++ lab_04/src/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
 #include "lab_04.h"
-
+/*#include "GenCode.cpp"
+#include "Lexem.cpp"
+#include "Lexer.cpp"
+#include "Syntax.cpp"
+#include "Tree.cpp"
+#include "Variable.cpp"*/
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::string path = "test.p";
     Parse4(path);
-
     return 0;
 }
